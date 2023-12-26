a0a288d0c9e76565607e22f7ae08dcd9b2bfdaf3
Anushka Kundu <Anushka Kundu>
2023-12-26 23:35:55 +0530

    first commit

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+import '../sign_in/sign_in_screen.dart';
+import 'components/splash_content.dart';
+
+class SplashScreen extends StatefulWidget {
+  static String routeName = "/splash";
+
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int currentPage = 0;
+  List<Map<String, String>> splashData = [
+    {
+      "text": "\nWelcome to Liberate, \nLet’s liberate you from distractions!",
+      "image": "assets/images/screen1.png"
+    },
+    {
+      "text":
+          "\nGet alerts when you are distracted. \nSo that you can focus on things that matter.",
+      "image": "assets/images/screen2.png"
+    },
+    {
+      "text":
+          "\nAsk your friend to be your mentor! \n\nAfter you have set your limits on app usages, \nonly your mentor can allow you access. \nHolding you accountable!",
+      "image": "assets/images/screen3.png"
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: PageView.builder(
+                  onPageChanged: (value) {
+                    setState(() {
+                      currentPage = value;
+                    });
+                  },
+                  itemCount: splashData.length,
+                  itemBuilder: (context, index) => SplashContent(
+                    image: splashData[index]["image"],
+                    text: splashData[index]['text'],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: <Widget>[
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          splashData.length,
+                          (index) => AnimatedContainer(
+                            duration: kAnimationDuration,
+                            margin: const EdgeInsets.only(right: 5),
+                            height: 6,
+                            width: currentPage == index ? 20 : 6,
+                            decoration: BoxDecoration(
+                              color: currentPage == index
+                                  ? kPrimaryColor
+                                  : const Color(0xFFD8D8D8),
+                              borderRadius: BorderRadius.circular(3),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const Spacer(flex: 3),
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, SignInScreen.routeName);
+                        },
+                        child: const Text("Continue"),
+                      ),
+                      const Spacer(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
